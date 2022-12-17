0c76e7e136aa8226861d3a41df134ffb843adcb0
Leonardo Domingues <leodomiings@gmail.com>
2022-12-16 22:57:37 -0300

    feat : splash screen

--- app/src/main/java/com/uruklabs/dictionaryapp/MainActivity.kt
+++ app/src/main/java/com/uruklabs/dictionaryapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.uruklabs.dictionaryapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar)).let {
             supportActionBar?.title = null
